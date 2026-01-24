ab249d603a901f5dd971dad615523d6406a7243f
Iggo Ferreira <virggodepaulo@gmail.com>
2026-01-24 16:41:02 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -90,6 +90,12 @@ export default function Home() {
                     Consultar Protocolo
                   </Button>
                 </Link>
+                <a href="https://www.participa.df.gov.br/static/orientacao-manifestacao" target="_blank" rel="noopener noreferrer">
+                  <Button size="lg" variant="outline" className="w-full sm:w-auto border-white/30 text-white hover:bg-white/10 h-14 px-8">
+                    <HelpCircle className="w-5 h-5 mr-2" />
+                    Orientações para seu registro
+                  </Button>
+                </a>
               </div>
             </div>
             <div className="hidden md:flex justify-center">
